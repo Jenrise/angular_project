19f7ef4ca91af6eba0425bd680dc0c49d9d9933f
Alisa <alice.jenrise@gmail.com>
2019-12-16 17:18:30 +0300

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyA4WReov__6chZVq8hY4Xdhz3H6F_Yw2wk',
+  fbDbUrl: 'https://angular-blog-32f29.firebaseio.com'
+};
